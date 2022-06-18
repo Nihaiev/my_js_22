a0268cbbb664f7f5477cbbdadd45f40f5e81e9a0
Иван Нигаев <Nihaiev@it.group>
2022-06-18 15:05:38 +0300

    added js files

---
+++ js/script1.js
@@ -0,0 +1,7 @@
+const a = 'Переменная "а" в script-1.js';
+
+console.log(a);
+
+const c = 'Переменная "c" в script-1.js'
+
+console.log(c);
---
+++ js/vars.js
@@ -0,0 +1,8 @@
+const age = 10;
+const totalPrice = 200.74;
+const userName = 'Chelsy';
+const message = 'Добро пожаловать!';
+const isOpen = true;
+const shouldConfirm = false;
+
+console.log(totalPrice);
