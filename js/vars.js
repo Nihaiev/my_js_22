3fc864715db7fe27e26acbc580c08026bdd78214
Иван Нигаев <Nihaiev@it.group>
2022-06-18 18:04:57 +0300

    numbers

--- js/vars.js
+++ js/vars.js
@@ -5,8 +5,10 @@ const message = 'Добро пожаловать!';
 const isOpen = true;
 const shouldConfirm = false;
 
-const type = typeof true;
-console.log(type);
+alert('qwerty');
+
+// const type = typeof true;
+// console.log(type);
 
 // console.log(totalPrice); метод викликає аргумент зі скобок в консоль
 
