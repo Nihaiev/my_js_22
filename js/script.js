18efaa723137b5964306576384b07f40dbdb85f0
Ivan <nigaev.ivan@gmail.com>
2022-06-22 01:14:38 +0300

    example

---
+++ js/script.js
@@ -0,0 +1,32 @@
+// function multiply(x, y, z) {
+// //   console.log("Код до return выполняется как обычно");
+
+//   // Возвращаем результат выражения умножения
+//   return x * y * z;
+
+// //   console.log("Этот лог никогда не выполнится, он стоит после return");
+// }
+
+// // Результат работы функции можно сохранить в переменную
+// let result = multiply(2, 3, 5);
+// console.log(result); // 30
+
+// result = multiply(4, 8, 12);
+// console.log(result); // 384
+
+// result = multiply(17, 6, 25);
+// console.log(result); // 2550
+
+
+function add(a, b, c) {
+  // Change code below this line
+return a * b * c;
+
+  // Change code above this line
+}
+
+add(2, 5, 8); // 15
+
+console.log(add(15, 27, 10));
+console.log(add(10, 20, 30));
+console.log(add(5, 10, 15));
