8f67be7d4cd582b71e1d9a394dc2c461d8b252a2
Иван Нигаев <Nihaiev@it.group>
2022-06-25 16:50:33 +0300

    Update switch2.js

--- js/switch2.js
+++ js/switch2.js
@@ -19,29 +19,63 @@ switch (значення) {
 Оператор break в кінці кожного блоку case необхідний, щоб перервати подальші перевірки та одразу перейти до коду після switch у тому разі, коли перевірка на рівність повернула true.
 */
 
-function getSubscriptionPrice(type) {
-  let price;
+// function getSubscriptionPrice(type) {
+//   let price;
+//   // Change code below this line
+
+//  switch (type) { // Change this line
+//     case "starter": // Change this line
+//       price = 0; // Change this line
+//       break;
+
+//     case "professional": // Change this line
+//       price = 20; // Change this line
+//       break;
+
+//     case "organization": // Change this line
+//       price = 50; // Change this line
+//       break;
+//   }
+
+//   // Change code above this line
+//   return price;
+// }
+
+// console.log(getSubscriptionPrice("professional"));
+// console.log(getSubscriptionPrice("organization"));
+// console.log(getSubscriptionPrice("starter"));
+
+
+function checkPassword(password) {
+  const ADMIN_PASSWORD = "jqueryismyjam";
+  let message;
   // Change code below this line
 
- switch (type) { // Change this line
-    case "starter": // Change this line
-      price = 0; // Change this line
+  // if (password === null) {
+  //   message = "Canceled by user!";
+  // } else if (password === ADMIN_PASSWORD) {
+  //   message = "Welcome!";
+  // } else {
+  //   message = "Access denied, wrong password!";
+  // }
+
+   switch (password) { // Change this line
+    case null: // Change this line
+      message = "Canceled by user!"; // Change this line
       break;
 
-    case "professional": // Change this line
-      price = 20; // Change this line
+    case ADMIN_PASSWORD: // Change this line
+      message = "Welcome!"; // Change this line
       break;
+default: message ="Access denied, wrong password!"
 
-    case "organization": // Change this line
-      price = 50; // Change this line
-      break;
   }
 
   // Change code above this line
-  return price;
+  return message;
 }
 
-console.log(getSubscriptionPrice("professional"));
-console.log(getSubscriptionPrice("organization"));
-console.log(getSubscriptionPrice("starter"));
-
+console.log(checkPassword("mangohackzor"));
+console.log(checkPassword(null));
+console.log(checkPassword("polyhax"));
+console.log(checkPassword("jqueryismyjam"));
