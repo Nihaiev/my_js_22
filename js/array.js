9644a067bc58a71cd980f7423b9f697cccd6e9ee
Иван Нигаев <Nihaiev@it.group>
2022-06-27 00:15:41 +0300

    Update array.js

--- js/array.js
+++ js/array.js
@@ -1,10 +1,22 @@
 // Масив
 
-// const =[]; - просто змінній ставимо квадратні дужки, це називається літерал масиву 
+// const =[]; - просто змінній ставимо квадратні дужки, це називається літерал масиву
 
-const friends =['Mango', 'Kiwi', 'Poly', 'Ajax'];
-console.table(friends);
-// const LastIndex = friends.length - 1; //виразовуємо останній індекс масиву (масив буде динамічний)
-friends[1] = 'qwerty'; // достукались до 1 значення масиву( починається з 0) і переоприділили його 
-// console.log(LastIndex);
-console.table(friends);
+// const friends =['Mango', 'Kiwi', 'Poly', 'Ajax'];
+// console.table(friends);
+// // const LastIndex = friends.length - 1; //виразовуємо останній індекс масиву (масив буде динамічний)
+// friends[1] = 'qwerty'; // достукались до 1 значення масиву( починається з 0) і міняємо в памяті кусок масиву
+// // console.log(LastIndex);
+// console.table(friends);
+
+// Передача по посиланню і по значенню
+
+const a = [1, 2, 3];
+const b = a;
+console.log('a', a);
+console.log('b', b);
+
+a[0] = 500;
+
+console.log('a', b);
+console.log('b', b);
