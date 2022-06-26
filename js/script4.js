d675f58322d29f2e79fc2db08f59d260a60cda14
Ivan <nigaev.ivan@gmail.com>
2022-06-26 18:39:17 +0300

    update

---
+++ js/script4.js
@@ -0,0 +1,22 @@
+const input = Math.ceil(Number(prompt("Введіть поверх")));
+console.log(input);
+const freePrice = 0;
+const lowPrice = 20;
+const middlePrice = 30;
+const highPrice = 40;
+let = totalPrice = 0;
+let message = `Введіть піднятті на ${i} поверх ${totalPrice}`;
+
+
+if (isNaN(input)) {
+    alert("Ви ввели не число");
+}
+
+for (let i = 1; i <= input; i + -1){
+    if (input === 1) {
+        message = "Вартість доставки";
+        break;
+    }
+
+}
+
