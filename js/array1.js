d033ae7d9a38a17ce6a6ac2a1723051e5921f37f
Иван Нигаев <Nihaiev@it.group>
2022-06-27 23:47:47 +0300

    array iterator

---
+++ js/array1.js
@@ -0,0 +1,12 @@
+//* Перебор масиву (ітерація)
+// - for - якщо потрібен індекс або змінити елемент масиву
+// - for...of - якщо індекс не потрібен і в масиві нічого міняти
+
+const friends = ['Mango', 'Kiwi', 'Poly', 'Ajax'];
+console.table(friends);
+const lastIndex = friends.length - 1;
+
+for (let i = 0; i <= lastIndex; i += 1) {
+    // console.log(i);
+    console.log(friends[i]);
+}
