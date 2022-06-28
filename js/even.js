9c8529b827ed90928fc761a21326f331bab2e731
Иван Нигаев <Nihaiev@it.group>
2022-06-29 00:46:24 +0300

    even

--- js/even.js
+++ js/even.js
@@ -11,13 +11,25 @@ const numbers = [1, 5, 8, 9, 12, 4, 15, 27, 30, 18, 11];
 let total = 0;
 
 // 2. перебрати масив
-for (let i = 0; i < numbers.length; i += 1){
-    console.log(numbers[i]);
-    // 3. на кожній ітерації перевірити елемент на парність
-if (numbers[i] % 2 === 0) {
+// for (let i = 0; i < numbers.length; i += 1){
+//     const number = numbers[i];
+//     console.log(number);
+//     // 3. на кожній ітерації перевірити елемент на парність
+// if (number % 2 === 0) {
+//     console.log('Парне!');
+//     // 4. якщо парне + до тотал
+//     total += number;
+// }
+// }
+
+
+// for of без ітератора і без запису в комірку памяті (for of зручний і швидкий)
+for (const number of numbers) {
+    // console.log(number);
+    if (number % 2 === 0) {
     console.log('Парне!');
     // 4. якщо парне + до тотал 
-    total += numbers[i];
+    total += number;
 }
 }
 
