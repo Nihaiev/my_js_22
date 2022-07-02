61ee12e7c7f06cb09a4e1cab2d6f6d883e39a9e3
Иван Нигаев <Nihaiev@it.group>
2022-07-02 10:22:57 +0300

    Update array2.js

--- js/array2.js
+++ js/array2.js
@@ -16,11 +16,47 @@
 *lastThreeEls - масив із трьох останніх елементів
 */
 
-const fruits = ['apple', 'plum', 'pear', 'orange', 'banana'];
+// const fruits = ['apple', 'plum', 'pear', 'orange', 'banana'];
 
-// Change code below this line
-const firstTwoEls = fruits.slice(0, 2);
-const nonExtremeEls = fruits.slice(1, -1);
-const lastThreeEls = fruits.slice(-3);
+// // Change code below this line
+// const firstTwoEls = fruits.slice(0, 2);
+// const nonExtremeEls = fruits.slice(1, -1);
+// const lastThreeEls = fruits.slice(-3);
 
-console.log(nonExtremeEls);
+// console.log(nonExtremeEls);
+
+// function checkFruit(fruit) {
+//   const fruits = ["apple", "plum", "pear", "orange"];
+
+// fruits.includes(fruit);
+
+//   return fruit; // Change this line
+// }
+// console.log(checkFruit("plum"));
+// console.log(checkFruit("mandarin"));
+// console.log(checkFruit("pear"));
+// console.log(checkFruit("Pear"));
+// console.log(checkFruit("apple"));
+
+/*Спільними елементами масивів називають ті елементи, які присутні у всіх масивах.
+*Наприклад, у двох масивах [1, 3, 5] і [0, 8, 5, 3] спільними будуть числа 3 і 5, оскільки вони присутні в обох вихідних масивах. А числа 0, 1 і 8 присутні тільки в одному з масивів.
+*Напиши функцію getCommonElements(array1, array2), яка отримує два масиви довільної довжини в параметри array1 і array2, і повертає новий масив, що складається з тих елементів, які присутні в обох вихідних масивах.
+*
+*
+*
+*
+*
+*
+*/
+
+function getCommonElements(array1, array2) {
+  const uniqieElements = [];
+  for (const element of array1) {
+    if (array2.includes(element)) {
+      uniqieElements.push(element);
+    }
+  }
+  return uniqieElements;
+}
+
+console.log(getCommonElements([1, 2, 3], [2, 4]));
