6ce3a6821ed82c129e77be11d71bf547df79b4a5
Иван Нигаев <Nihaiev@it.group>
2022-07-03 16:03:58 +0300

    Stack trace

--- js/func.js
+++ js/func.js
@@ -6,12 +6,21 @@ const add = function (x,y) {
 console.log('Виконується функція add');
     // const result = x + y;
     // console.log('x+y: ', result);
-    return x + y;
+    return x + y; // повернення результату з функції 
     
 }
 
-add(2, 3);// виклик функції (2.3 - передаємо аргументи, параметри x,y)
-add(5, 15);
-add(20, 5);
+// add(2, 3);// виклик функції (2.3 - передаємо аргументи, параметри x,y)
+// add(5, 15);
+// add(20, 5);
 
-1336
+const r1 = add(5, 3);
+console.log('r1: ', r1);
+
+const r2 = add(10, 15);
+console.log('r2: ', r2);
+
+const r3 = add(30, 50);
+console.log('r3: ', r3);
+
+
---
+++ js/stacktrace.js
@@ -0,0 +1,21 @@
+/* Стек викликів, механіз завдяки якому працюють функції в js
+* - Stack trace
+*
+*
+*
+*/
+
+const fnA = function () {
+    console.log('Виконується функція А');
+}
+
+const fnB = function () {
+    console.log('Виконується функція B');
+}
+
+const fnC = function () {
+    console.log('Виконується функція C');
+}
+fnA();
+fnB();
+fnC();
