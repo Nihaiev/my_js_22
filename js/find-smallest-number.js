b2acf400b8a334bd10c4b935b1aa427875dec20c
Иван Нигаев <Nihaiev@it.group>
2022-07-03 23:54:41 +0300

    slugify

---
+++ js/find-smallest-number.js
@@ -0,0 +1,12 @@
+//функція для пошуку самого маленького числа в масиві
+
+const numbers = [51, 18, 13, 24, 7, 85, 19];
+let smallestNumber = numbers[0];
+
+for (const number of numbers) {
+    if (number < smallestNumber) {
+        smallestNumber = number;
+    }
+}
+
+console.log('smallestNumber: ', smallestNumber)
