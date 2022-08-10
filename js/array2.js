9ff45c3420574febdbe8fec5ea81276b7675082f
Иван Нигаев <Nihaiev@it.group>
2022-08-10 18:39:19 +0300

    update

--- js/array2.js
+++ js/array2.js
@@ -76,14 +76,14 @@
 // console.log(calculateTotalPrice([]));
 // console.log(calculateTotalPrice());
 
-const a = 3 % 3;
-const b = 4 % 3;
-const c = 11 % 4;
-const d = 12 % 7;
-const e = 8 % 3;
+// const a = 3 % 3;
+// const b = 4 % 3;
+// const c = 11 % 4;
+// const d = 12 % 7;
+// const e = 8 % 3;
 
-console.log(a);
-console.log(b);
-console.log(c);
-console.log(d);
-console.log(e);
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+// console.log(e);
