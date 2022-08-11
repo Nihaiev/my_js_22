c357b9aa0dd20d9a589b38c1c438f92b7bc74dcd
Ivan <nigaev.ivan@gmail.com>
2022-08-11 15:54:30 +0300

    array 3

---
+++ js/array3.js
@@ -0,0 +1,18 @@
+// const friends = ['Mango', 'Kiwi', 'Poly', 'Ajax'];
+// // console.table(friends);
+// const lastIndex = friends.length - 1;
+
+// // for (let i = 0; i <= friends.length - 1; i += 1){
+// //     console.log(friends[i]);
+    
+// // }
+
+// for (const friend of friends) {
+//     console.log(friend);
+// }
+
+
+/**
+ * Порахувати загальну сумму
+ */
+//todo 48
